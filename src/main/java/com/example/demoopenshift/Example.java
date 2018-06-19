0f7510ddf4e0ee3b400265e6cb77ca3ad120214a
amalvijayvr <amalvijayvr.developer@gmail.com>
2018-06-19 14:58:32 +0530

    first commit

--- src/main/java/com/example/demoopenshift/Example.java
+++ src/main/java/com/example/demoopenshift/Example.java
@@ -11,7 +11,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from Spring boot!";
+        return "ADM UK Dev Practices Test !!!!";
     }
 
     public static void main(String[] args) throws Exception {
